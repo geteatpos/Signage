6e5282934b0fa236e5541e09ee4ea22b9c4fdc25
Dannover Arroyave <dannover@gmail.com>
2022-11-09 19:22:50 -0500

    commit

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=c:\Developer\flutter"
-export "FLUTTER_APPLICATION_PATH=c:\Temp2\signage"
+export "FLUTTER_APPLICATION_PATH=C:\Developer\flutterdev\signage"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
